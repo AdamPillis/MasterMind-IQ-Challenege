14ce6503e9ccee09759122f6bdadf0ab1862695c
AdamPillis <adam.pillis@hotmail.com>
2021-10-06 17:01:58 +0000

    Start: function that handles the submit button in start-screen

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let form = document.getElementById('login-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    let player = form.elements.playername.value;
+   
+}

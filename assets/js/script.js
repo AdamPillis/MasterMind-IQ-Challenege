0918dced4040d4b71ebdffcca7d6e4d901f41623
AdamPillis <adam.pillis@hotmail.com>
2021-10-07 10:21:26 +0000

    Add: function to call menu screen and player name

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ function handleSubmit(event) {
 
     event.preventDefault();
 
-    let player = form.elements.playername.value;
-   
+    let player = form.elements.playername;
+   let menuScreen = document.getElementById('menu-screen');
+   let startScreen = document.getElementById('start-screen')
+   let welcomeMessage = document.getElementsByClassName('welcome-message')[0];
+
+   welcomeMessage.innerText = `Welcome to MasterMindsIQ ${player.value} !`;
+
+   startScreen.style.display = 'none';
+   menuScreen.style.display = 'unset';
 }

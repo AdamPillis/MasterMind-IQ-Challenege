4b3f2330c8f72716aa30ce69b86e69301d8655ad
AdamPillis <adam.pillis@hotmail.com>
2021-10-08 13:27:56 +0000

    Add: displayRules function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,32 +19,73 @@ function handleSubmit(event) {
      startScreen.style.display = 'none';
      menuScreen.style.display = 'unset';
 }
+// blank variable to which the checkDifficultyLevel put difficulty level
+let difficultyLevel = '';
+/** to check which radio button is 'checked'
+ * different string for each level which is placed into the difficultyLevel variable. 
+ */
+function checkDifficultyLevel() {
+    
+    let easyButton = document.getElementById('easy-level');
+    let mediumButton = document.getElementById('medium-level');
+    let hardButton = document.getElementById('hard-level');
 
-    let easyButton = document.getElementById('easy-button');
-    let mediumButton = document.getElementById('medium-button');
-    let hardButton = document.getElementById('hard-button');
-
-    easyButton.addEventListener('click', easyLevel);
-    mediumButton.addEventListener('click', mediumLevel);
-    hardButton.addEventListener('click', difficultyLevel);
-
-function easyLevel() {
-
-    if(easyButton) {
-        easyButton.style.backgroundColor = 'red';
-        alert('You clicked easy');
-        } else {
-             alert("Unknown difficulty level");
+    if (easyButton.checked) {
+        difficultyLevel = 'easy';
+    } else if (mediumButton.checked) {
+            difficultyLevel = 'medium';
+        } else if (hardButton.checked) {
+                difficultyLevel = 'hard'; }
+            else {
+                alert("Unknown difficulty level");
                 }
     }
-function mediumLevel() {
+// call category buttons list by class name
+let categoryButtons = document.getElementsByClassName('answers-area');
+// indexing movies button and adding a click event listener to it
+let moviesButton = categoryButtons[0];
+moviesButton.addEventListener('click', checkMoviesLevel);
+/** checkMoviesLevel() checks what the difficulty level is 
+ * and calls an individual function for each category level
+ */
+function checkMoviesLevel() {
+    checkDifficultyLevel(difficultyLevel);
 
-    if(mediumButton) {
-        mediumButton.style.backgroundColor = 'blue';
-            alert('You clicked medium');
+    if (difficultyLevel == 'easy') {
+        alert('You chose easy movies');
+        displayRules();
+    } else if (difficultyLevel == 'medium') {
+            alert('You chose medium movies');
+        } else if (difficultyLevel == 'hard') {
+            alert('You chose hard movies');
         } else {
-            alert("Unknown difficulty level");
-            }
+            alert('Unknown category level');
         }
+    }
+/**
+ * displayRules function hides menu screen and calls rules screen to display
+ */
+function displayRules() {
+    
+    let menuScreen = document.getElementById('menu-screen');
+    let rulesScreen = document.getElementById('rules-screen');
+    let rulesButton = document.getElementById('rules-button');
+    let gameScreen = document.getElementById('game-screen');
+    rulesButton.addEventListener('click', )
+
+     menuScreen.style.display = 'none';
+     rulesScreen.style.display = 'unset';
+    
+     if (rulesButton.click) {
+        rulesScreen.style.display = 'none';
+        gameScreen.style.display = 'unset';
+     } else {
+         alert('Unable to load game');
+     }
+}
+/**
+ * displayEasyMovies function displays easy movies questions in order
+ */
+
 
 

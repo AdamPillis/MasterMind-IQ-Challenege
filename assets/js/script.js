df84e54f84d938982b73089b033cdb48084f9789
AdamPillis <adam.pillis@hotmail.com>
2021-10-08 14:20:51 +0000

    Add: displayEasyFunction to script.js + data

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,7 @@ function checkMoviesLevel() {
     checkDifficultyLevel(difficultyLevel);
 
     if (difficultyLevel == 'easy') {
-        alert('You chose easy movies');
+
         displayRules();
     } else if (difficultyLevel == 'medium') {
             alert('You chose medium movies');
@@ -69,23 +69,109 @@ function displayRules() {
     
     let menuScreen = document.getElementById('menu-screen');
     let rulesScreen = document.getElementById('rules-screen');
-    let rulesButton = document.getElementById('rules-button');
-    let gameScreen = document.getElementById('game-screen');
-    rulesButton.addEventListener('click')
 
      menuScreen.style.display = 'none';
      rulesScreen.style.display = 'unset';
-    
-     if (rulesButton.click) {
-        rulesScreen.style.display = 'none';
-        gameScreen.style.display = 'unset';
-     } else {
-         alert('Unable to load game');
-     }
+}
+// click event listener attached to button in rules screen section
+let rulesButton = document.getElementById('rules-button');
+rulesButton.addEventListener('click', hideRules);
+/**
+ * hide rules screen and load game screen section
+ */
+function hideRules() {
+    let rulesScreen = document.getElementById('rules-screen');
+    let gameScreen = document.getElementById('game-screen');
+
+    rulesScreen.style.display = 'none';
+    gameScreen.style.display = 'unset';
 }
 /**
  * displayEasyMovies function displays easy movies questions in order
  */
-
+function displayEasyMovies() {
+    let easyMovieQuestions = [
+        {
+            question: "Which film won the Oscar for ‘best picture’ in 2019?",
+            optionA: "A Star is Born",
+            optionB: "Vice",
+            optionC: "Green Book",
+            optionD: "The Favourite",
+            Answer: "Green Book"
+        },
+        {
+            question: "Jurassic Park is based on the novel by which author?",
+            optionA: "Stephen King",
+            optionB: "John Grisham",
+            optionC: "Michael Crichton",
+            optionD: "Harlan Coben",
+            Answer: "Michael Crichton"
+        },
+        {
+            question: "Home Alone is set in which city?",
+            optionA: "Boston",
+            optionB: "New York",
+            optionC: "Chicago",
+            optionD: "Pittsburgh",
+            Answer: "Chicago"
+        },
+        {
+            question: "The gang members in Reservoir Dogs were given names based on what?",
+            optionA: "Months",
+            optionB: "Numbers",
+            optionC: "Presidents",
+            optionD: "Colours",
+            Answer: "Colours"
+        },
+        {
+            question: "When was the first Star Wars film released?",
+            optionA: 1970,
+            optionB: 1977,
+            optionC: 1985,
+            optionD: 1991,
+            Answer: 1977
+        },
+        {
+            question: "Who directed the film Alien?",
+            optionA: "Ridley Scott",
+            optionB: "James Cameron",
+            optionC: "Brian De Palma",
+            optionD: "Oliver Stone",
+            Answer: "Ridley Scott"
+        },
+        {
+            question: "Uma Thurman and John Travolta had an iconic dance scene in which movie?",
+            optionA: "Saturday Night Fever",
+            optionB: "Face Off",
+            optionC: "Pulp Fiction",
+            optionD: "Kill Bill",
+            Answer: "Pulp Fiction"
+        },
+        {
+            question: "Who played James Bond in ‘You Only Live Twice’?",
+            optionA: "Sean Connery",
+            optionB: "Roger Moore",
+            optionC: "Timothy Dalton",
+            optionD: "George Lazenby",
+            Answer: "Sean Connery"
+        },
+        {
+            question: "For what movie did Tom Hanks get his first Academy Award nomination?",
+            optionA: "Saving Private Ryan",
+            optionB: "Big",
+            optionC: "Forrest Gump",
+            optionD: "Cast Away",
+            Answer: "Big"
+        },
+        {
+            question: "In which year was ‘E.T the Extra Terrestrial’ released?",
+            optionA: 1962,
+            optionB: 1972,
+            optionC: 1982,
+            optionD: 1992,
+            Answer: 1982
+        },
+    ];
+}
 
 
